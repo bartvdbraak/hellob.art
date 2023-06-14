667b4ec2b1d009ec2ca375aabb7187f522b0edc2
Bart van der Braak <bartvdbraak@gmail.com>
2023-06-14 17:08:14 +0200

    fix: resolved issues with external links

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   url: "https://hellob.art",
   ogImage: "https://hellob.art/og.jpg",
   links: {
-    linkedIn: "https://linkedin.com/bartvdbraak",
+    linkedIn: "https://linkedin.com/in/bartvdbraak",
     github: {
       project: "https://github.com/bartvdbraak/hellob.art",
       profile: "https://github.com/bartvdbraak",

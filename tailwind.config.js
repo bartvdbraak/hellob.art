25d8b64ebf791126f57e440617df8e1d3763226e
Bart van der Braak <bartvdbraak@gmail.com>
2023-06-14 03:48:37 +0200

    feat: add filter to `backgroundImage`

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        filter: "brightness(120%) saturate(120%) blur(2px)",
       },
     },
   },

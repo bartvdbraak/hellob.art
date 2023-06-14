62656fa96393a9e1a9e7ee26145a3f8dd9c05997
Bart van der Braak <bartvdbraak@gmail.com>
2023-06-14 16:33:08 +0200

    feat: reworked page and components

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "Taxonomy",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js 13.",
+  url: "https://hellob.art",
+  ogImage: "https://hellob.art/og.jpg",
+  links: {
+    linkedIn: "https://linkedin.com/bartvdbraak",
+    github: {
+      project: "https://github.com/bartvdbraak/hellob.art",
+      profile: "https://github.com/bartvdbraak",
+    },
+  },
+};

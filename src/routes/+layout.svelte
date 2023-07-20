a00b7d66d9b58aa365fa0bd119de7b16a70e2b0e
Bart van der Braak <bartvdbraak@gmail.com>
2023-07-20 08:57:28 +0200

    feat: added layout and component for navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<!-- __layout.svelte -->
+<script>
+  import Nav from '../components/Nav.svelte';
+
+  export let routes = [
+    { url: "/", label: "Home" },
+    { url: "/projects", label: "Projects" },
+    { url: "/about", label: "About" },
+    // { url: "/blog", label: "Blog" },
+    { url: "/contact", label: "Contact" }
+  ]
+</script>
+
+<main>
+  <Nav {routes} />
+  <slot />
+</main>

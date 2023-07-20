4c99a08fcc57896cc1d135d661e582feabf4b0e8
Bart van der Braak <bartvdbraak@gmail.com>
2023-07-20 09:13:07 +0200

    refactor: remove tmi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import { calculateAge } from "$lib/calculate-age";
 
   export let age = calculateAge('1994-10-18');
-  export let girlfriendDuration = calculateAge('2011-08-08');
   export let location = "Zaandam, Netherlands";
   export let loves = "cats and whiskey";
   export let passion = "solving problems with code and automation";
@@ -10,7 +9,7 @@
 
 <main>
   <h1>Hello, I'm Bart van der Braak!</h1>
-  <p>I'm {age} years old and have been with my girlfriend for {girlfriendDuration} years.</p>
+  <p>I'm {age} years old</p>
   <p>I'm located in {location}.</p>
   <p>I love {loves}.</p>
   <p>I'm passionate about {passion}.</p>

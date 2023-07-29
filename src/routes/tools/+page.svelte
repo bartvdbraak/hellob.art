9827c6039c12e0815638397acc35eeff4d8c0738
Bart van der Braak <bartvdbraak@gmail.com>
2023-07-30 00:35:52 +0200

    feat: add orbit control scene without transform

--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,7 +1,9 @@
 <!-- src/routes/tools/+page.svelte -->
 <script lang="ts">
 	import { Canvas } from '@threlte/core';
-	import Scene from './Scene.svelte';
+	import { T } from '@threlte/core';
+	import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras';
+	import Github from './Github.svelte';
 </script>
 
 <svelte:head>
@@ -11,7 +13,51 @@
 <main class="container mx-auto px-4 py-8 text-left">
 	<h2 class="text-3xl font-bold mb-8">Tools</h2>
 
-	<Canvas>
-		<Scene />
+	<Canvas size={{ width: 800, height: 800 }}>
+		<T.PerspectiveCamera makeDefault position={[-10, 10, 40]} fov={15}>
+			<OrbitControls
+				autoRotate
+				enableZoom={false}
+				enableDamping
+				autoRotateSpeed={0.5}
+				target.y={3}
+			/>
+		</T.PerspectiveCamera>
+
+		<T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
+		<T.AmbientLight intensity={0.2} />
+
+		<Grid
+			position.y={-0.001}
+			cellColor="#ffffff"
+			sectionColor="#ffffff"
+			sectionThickness={0}
+			fadeDistance={50}
+			cellSize={2}
+		/>
+
+		<ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
+
+		<Float floatIntensity={1} floatingRange={[0, 1]}>
+			<T.Mesh position.y={1.2} position.z={-0.75}>
+				<T.BoxGeometry />
+				<T.MeshStandardMaterial color={[0.2, 0.2, 0.5]} />
+			</T.Mesh>
+		</Float>
+
+		<Float floatIntensity={1} floatingRange={[0, 1]}>
+			<T.Mesh position={[1.2, 1.5, 0.75]} rotation.x={5} rotation.y={71}>
+				<T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
+				<T.MeshStandardMaterial color={[0.5, 0.9, 0.5]} />
+			</T.Mesh>
+		</Float>
+
+		<Float floatIntensity={1} floatingRange={[0, 1]}>
+			<!-- <T.Mesh position={[-1.4, 1.5, 0.75]} rotation={[-5, 128, 10]}>
+				<T.IcosahedronGeometry />
+				<T.MeshStandardMaterial color={[0.2, 0.9, 0.8]} />
+			</T.Mesh> -->
+			<Github />
+		</Float>
 	</Canvas>
 </main>

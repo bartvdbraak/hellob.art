1cef49241dd17bab9deb01a94fa9cd721fb1d73f
Bart van der Braak <bartvdbraak@gmail.com>
2023-07-30 02:15:22 +0200

    feat: added icons of certain tools

--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -4,6 +4,8 @@
 	import { T } from '@threlte/core';
 	import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras';
 	import Github from './Github.svelte';
+	import TerraformFlat from './TerraformFlat.svelte';
+	import Kubernetes from './Kubernetes.svelte';
 </script>
 
 <svelte:head>
@@ -13,18 +15,18 @@
 <main class="container mx-auto px-4 py-8 text-left">
 	<h2 class="text-3xl font-bold mb-8">Tools</h2>
 
-	<Canvas size={{ width: 800, height: 800 }}>
-		<T.PerspectiveCamera makeDefault position={[-10, 10, 40]} fov={15}>
+	<Canvas>
+		<T.PerspectiveCamera makeDefault position={[-10, 10, 30]} fov={15}>
 			<OrbitControls
 				autoRotate
 				enableZoom={false}
 				enableDamping
-				autoRotateSpeed={0.5}
+				autoRotateSpeed={0.2}
 				target.y={3}
 			/>
 		</T.PerspectiveCamera>
 
-		<T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
+		<T.DirectionalLight intensity={0.5} position.x={5} position.y={10} />
 		<T.AmbientLight intensity={0.2} />
 
 		<Grid
@@ -32,32 +34,22 @@
 			cellColor="#ffffff"
 			sectionColor="#ffffff"
 			sectionThickness={0}
-			fadeDistance={50}
+			fadeDistance={40}
 			cellSize={2}
 		/>
 
 		<ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
 
 		<Float floatIntensity={1} floatingRange={[0, 1]}>
-			<T.Mesh position.y={1.2} position.z={-0.75}>
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color={[0.2, 0.2, 0.5]} />
-			</T.Mesh>
+			<Github position.y={0.4} position.z={-0.75} scale={50} />
 		</Float>
 
 		<Float floatIntensity={1} floatingRange={[0, 1]}>
-			<T.Mesh position={[1.2, 1.5, 0.75]} rotation.x={5} rotation.y={71}>
-				<T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-				<T.MeshStandardMaterial color={[0.5, 0.9, 0.5]} />
-			</T.Mesh>
+			<TerraformFlat position.y={0.4} position.z={-4} scale={10} />
 		</Float>
 
 		<Float floatIntensity={1} floatingRange={[0, 1]}>
-			<!-- <T.Mesh position={[-1.4, 1.5, 0.75]} rotation={[-5, 128, 10]}>
-				<T.IcosahedronGeometry />
-				<T.MeshStandardMaterial color={[0.2, 0.9, 0.8]} />
-			</T.Mesh> -->
-			<Github />
+			<Kubernetes position.y={0.4} position.z={-2} position.x={-3} scale={100} />
 		</Float>
 	</Canvas>
 </main>

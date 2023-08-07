e2b5f1078bbbec89c1959aa2e5f3e47ddf1d8e04
Bart van der Braak <bartvdbraak@gmail.com>
2023-08-07 20:30:20 +0200

    feat: revamped toolbox page

---
+++ src/routes/toolbox/ToolCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let name: string;
+	export let title: string;
+	export let description: string;
+	export let logo: string;
+	export let toolUrl: string;
+</script>
+
+<a
+	href={toolUrl}
+	target="_blank"
+	rel="external"
+	class="mb-6 inline-block h-full w-full rounded-md border border-white/10 bg-white/5 p-4 shadow-xl duration-200 hover:bg-white/10 hover:shadow-2xl"
+>
+	<div class="flex flex-col gap-6">
+		<div class="flex gap-6">
+			<img src={logo} alt={name + ' logo'} class="h-12 w-12 rounded-sm object-contain" />
+			<div class="grow">
+				<h4 class="mb-0">{name}</h4>
+				<p class="text-faded text-sm font-normal">{title}</p>
+			</div>
+		</div>
+		<p class="text-faded mt-0 text-base font-normal">{description}</p>
+	</div>
+</a>
---
+++ src/routes/toolbox/tools-assets.ts
@@ -0,0 +1,99 @@
+import GitHubLogo from '$lib/assets/tools/github.svg';
+import OverleafLogo from '$lib/assets/tools/overleaf.svg';
+import ArgoCDLogo from '$lib/assets/tools/argocd.svg';
+import Studio3TLogo from '$lib/assets/tools/studio3t.svg';
+import WarpLogo from '$lib/assets/tools/warp.svg';
+import K9sLogo from '$lib/assets/tools/k9s.png';
+import FigmaLogo from '$lib/assets/tools/figma.svg';
+import AzureCLILogo from '$lib/assets/tools/azure-cli.svg';
+import KeystoreExplorer from '$lib/assets/tools/keystore-exporer.png';
+import TogglTrackLogo from '$lib/assets/tools/toggl-track.svg';
+import HelmLogo from '$lib/assets/tools/helm.svg';
+import RoyalTSXLogo from '$lib/assets/tools/royal-tsx.png';
+import PoetryLogo from '$lib/assets/tools/poetry.svg';
+import MaccyLogo from '$lib/assets/tools/maccy.png';
+import BalenaEtcherLogo from '$lib/assets/tools/balena-etcher.svg';
+import SendGridLogo from '$lib/assets/tools/sendgrid.svg';
+import InsomniaLogo from '$lib/assets/tools/insomnia.svg';
+import BlenderLogo from '$lib/assets/tools/blender.svg';
+import BitwardenLogo from '$lib/assets/tools/bitwarden.svg';
+import AzureDataStudioLogo from '$lib/assets/tools/azure-data-studio.png';
+import WiresharkLogo from '$lib/assets/tools/wireshark.png';
+import RectangleLogo from '$lib/assets/tools/rectangle.png';
+import PyCharmLogo from '$lib/assets/tools/pycharm.svg';
+import DockerLogo from '$lib/assets/tools/docker.webp';
+import CyberduckLogo from '$lib/assets/tools/cyberduck.png';
+import MullvadVPNLogo from '$lib/assets/tools/mullvad-vpn.svg';
+import JqLogo from '$lib/assets/tools/jq.svg';
+import AkamaiLogo from '$lib/assets/tools/akamai.svg';
+import CloudFlareLogo from '$lib/assets/tools/cloudflare.svg';
+import VisualStudioCodeLogo from '$lib/assets/tools/visual-studio-code.svg';
+import OpsGenieLogo from '$lib/assets/tools/opsgenie.svg';
+import VimLogo from '$lib/assets/tools/vim.svg';
+import NotionLogo from '$lib/assets/tools/notion.svg';
+import NvmLogo from '$lib/assets/tools/nvm.svg';
+import GIMPLogo from '$lib/assets/tools/gimp.svg';
+import AzureStorageExplorerLogo from '$lib/assets/tools/azure-storage-explorer.svg';
+import MeldLogo from '$lib/assets/tools/meld.svg';
+import KarabinerElementsLogo from '$lib/assets/tools/karabiner-elements.png';
+import KustomizeLogo from '$lib/assets/tools/kustomize.svg';
+import InkScapeLogo from '$lib/assets/tools/inkscape.svg';
+import OBSLogo from '$lib/assets/tools/obs.png';
+import WireGuardLogo from '$lib/assets/tools/wireguard.svg';
+import SwitchHostsLogo from '$lib/assets/tools/switchhosts.svg';
+import SlackLogo from '$lib/assets/tools/slack.svg';
+import NmapLogo from '$lib/assets/tools/nmap.svg';
+import DBeaverLogo from '$lib/assets/tools/dbeaver.svg';
+import TerraformLogo from '$lib/assets/tools/terraform.svg';
+import GitLensLogo from '$lib/assets/tools/gitlens.png';
+
+export {
+	GitHubLogo,
+	OverleafLogo,
+	ArgoCDLogo,
+	Studio3TLogo,
+	WarpLogo,
+	K9sLogo,
+	FigmaLogo,
+	AzureCLILogo,
+	KeystoreExplorer,
+	TogglTrackLogo,
+	HelmLogo,
+	RoyalTSXLogo,
+	PoetryLogo,
+	MaccyLogo,
+	BalenaEtcherLogo,
+	SendGridLogo,
+	InsomniaLogo,
+	BlenderLogo,
+	BitwardenLogo,
+	AzureDataStudioLogo,
+	RectangleLogo,
+	PyCharmLogo,
+	DockerLogo,
+	CyberduckLogo,
+	MullvadVPNLogo,
+	JqLogo,
+	AkamaiLogo,
+	CloudFlareLogo,
+	VisualStudioCodeLogo,
+	OpsGenieLogo,
+	VimLogo,
+	NotionLogo,
+	NvmLogo,
+	GIMPLogo,
+	AzureStorageExplorerLogo,
+	MeldLogo,
+	KarabinerElementsLogo,
+	KustomizeLogo,
+	InkScapeLogo,
+	OBSLogo,
+	WireGuardLogo,
+	SwitchHostsLogo,
+	WiresharkLogo,
+	SlackLogo,
+	NmapLogo,
+	DBeaverLogo,
+	TerraformLogo,
+	GitLensLogo
+};

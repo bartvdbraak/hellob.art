d63bee294bb3e9a1534837017a6817c53019219e
Bart van der Braak <bartvdbraak@gmail.com>
2023-11-20 19:06:17 +0100

    feat: error page and og image

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>hellob.art &mdash; {$page.error?.message.toLowerCase() || 'error'}</title>
+</svelte:head>
+
+<div class="h-screen flex justify-center items-center">
+	<h2 class="text-3xl font-bold mb-8">{$page.error?.message || 'An unknown error occurred.'}</h2>
+</div>

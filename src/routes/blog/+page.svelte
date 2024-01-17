292d8f58be6a666b5bb6151e402d8e7d6a0f09a0
Bart van der Braak <bartvdbraak@gmail.com>
2024-01-17 21:56:06 +0100

    feat: scaffold for contact and blog routes

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as PageHeader from '$lib/components/site/page-header';
+</script>
+
+<div class="container relative max-w-[980px]">
+	<PageHeader.Root class="pb-8">
+		<PageHeader.Heading>Written pieces</PageHeader.Heading>
+		<PageHeader.Description>
+			Stuff I've written for others to read, discuss, and hopefully enjoy.
+		</PageHeader.Description>
+	</PageHeader.Root>
+
+	<section></section>
+</div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as PageHeader from '$lib/components/site/page-header';
+</script>
+
+<div class="container relative max-w-[980px]">
+	<PageHeader.Root class="pb-8">
+		<PageHeader.Heading>Contact me</PageHeader.Heading>
+		<PageHeader.Description>
+			Even though I'm not a big fan of social media, I'm still reachable through.
+		</PageHeader.Description>
+	</PageHeader.Root>
+
+	<section></section>
+</div>

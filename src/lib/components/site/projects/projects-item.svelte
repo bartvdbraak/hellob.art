1fa52b84c1f25775a5a0609b7046791e58fca3f8
Bart van der Braak <bartvdbraak@gmail.com>
2024-01-23 18:10:45 +0100

    chore: added project content

--- src/lib/components/site/projects/projects-item.svelte
+++ src/lib/components/site/projects/projects-item.svelte
@@ -1,27 +1,23 @@
 <script lang="ts">
 	import { Circle } from 'radix-icons-svelte';
-	import { Scale } from 'lucide-svelte';
+	import { Calendar, Scale } from 'lucide-svelte';
 	import * as Card from '$lib/components/ui/card';
 	import type { Project } from '$lib/content/projects';
-	import * as Avatar from '$lib/components/ui/avatar';
 
 	export let projectsItem: Project;
-	let { avatarImg, title, description, languages, license, year } = projectsItem;
+	let { logo, title, description, languages, license, year } = projectsItem;
 </script>
 
 <Card.Root class="hover:bg-muted/50">
-	<Card.Header class="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
+	<Card.Header class="grid grid-cols-[1fr_72px] items-start gap-4 space-y-0">
 		<div class="space-y-1">
-			<Card.Title tag="h2">{title}</Card.Title>
+			<Card.Title tag="h2" class="mb-2">{title}</Card.Title>
 			<Card.Description>
 				{description}
 			</Card.Description>
 		</div>
 		<div class="justify-self-end">
-			<Avatar.Root>
-				<Avatar.Image src={avatarImg} alt={title} />
-				<Avatar.Fallback>NA</Avatar.Fallback>
-			</Avatar.Root>
+			<enhanced:img src={logo} class="rounded-sm" alt={`${title} logo`} />
 		</div>
 	</Card.Header>
 	<Card.Content>
@@ -36,8 +32,8 @@
 				<Scale class="mr-1 h-3 w-3" />
 				{license}
 			</div>
-			<div>
-				Started in
+			<div class="flex items-center">
+				<Calendar class="mr-1 h-3 w-3" />
 				{year}
 			</div>
 		</div>
